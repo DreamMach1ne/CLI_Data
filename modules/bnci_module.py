82f67750a8f8f3035d094b60dca16657802575a3
Hewlbern <michael.t.holborn@gmail.com>
2023-09-09 12:07:27 +1000

    weird

--- modules/bnci_module.py
+++ modules/bnci_module.py
@@ -1,10 +1,11 @@
-from extract.downloader import  download_all_files,prepare_download,fetch_bnci_links
+# modules/bnci_module.py
+from extract.downloader import download_all_files, prepare_download, fetch_bnci_links
 
-def download_from_bnci(base_url, destination_folder):
+def download_from_bnci(base_url, destination_folder, gcs_bucket=None):
     full_path, downloaded_files = prepare_download(destination_folder, "BNCI_Horizon_MAT_Datasets")
     download_links = fetch_bnci_links(base_url)
     if not download_links:
         print("[ERROR] No download links found.")
         return False
 
-    return download_all_files(download_links, full_path, downloaded_files)
+    return download_all_files(download_links, full_path, downloaded_files, gcs_bucket)
