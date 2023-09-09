a58a7098c430cccca36bcb683c9b4f54a37ab32c
Hewlbern <michael.t.holborn@gmail.com>
2023-09-09 12:21:17 +1000

    check for failrures

--- main.py
+++ main.py
@@ -20,50 +20,66 @@ def main():
         sources = ['gigadb', 'mindbigdata', 'bnci', 'figshare', 'direct', 'kaggle']
     else:
         sources = [args.source]
+    failed_sources = []
 
     for source in sources:
-        success = False
-        if source == 'bnci':
-            success = download_from_bnci("http://bnci-horizon-2020.eu/database/data-sets", destination_folder, args.gcs_bucket)
-        elif source == 'figshare':
-            success = download_from_figshare("https://api.figshare.com/v2/collections/3917698/articles", destination_folder, args.gcs_bucket)
-        elif source == 'mindbigdata':
-            success = download_from_mindnet(destination_folder, args.gcs_bucket)
-        elif source == 'gigadb':
-            success = download_from_gigadb("http://gigadb.org/dataset/view/id/100295", destination_folder, args.gcs_bucket)
-        elif source == 'direct':
-            direct_links = ['https://archive.ics.uci.edu/static/public/457/eeg+steady+state+visual+evoked+potential+signals.zip',
-            'https://www-ti.informatik.uni-tuebingen.de/~spueler/eeg_data/cVEP_dataset.rar',
-            'https://physionet.org/files/eegmmidb/1.0.0/','https://gin.g-node.org/robintibor/high-gamma-dataset/src/master/data/train/1.edf'
-            ,'https://archive.ics.uci.edu/static/public/230/planning+relax.zip',
-            'https://figshare.com/collections/A_large_electroencephalographic_motor_imagery_dataset_for_electroencephalographic_brain_computer_interfaces/3917698',
-            'http://www.enterface.net/enterface05/docs/results/databases/project1_database.zip',
-            'http://www.enterface.net/enterface05/docs/results/databases/project2_database.zip',
-            'http://www.enterface.net/enterface05/docs/results/databases/project5_database.zip',
-            'http://www.enterface.net/enterface05/docs/results/databases/project7_database.zip',
-            'https://headit.ucsd.edu/studies/3316f70e-35ff-11e3-a2a9-0050563f2612.jnlp',
-            'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-access-anchor',
-            'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-iv-access-anchor',
-            'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-vig-access-anchor',
-            'https://stacks.stanford.edu/file/druid:xd109qh3109/fhpred.zip',
-            "https://utexas.box.com/shared/static/7ab8qm5e3i0vfsku0ee4dc6hzgeg7nyh.zip",
-            'https://utexas.box.com/shared/static/3go1g4gcdar2cntjit2knz5jwr3mvxwe.zip',
-            "https://openneuro.org/datasets/ds003020/",
-            "https://utexas.box.com/shared/static/ae5u0t3sh4f46nvmrd3skniq0kk2t5uh.zip",
-            "https://openneuro.org/datasets/ds004510/",
-            "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz",
-            "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz"
-            ]
-            success = download_direct_links(direct_links, destination_folder, args.gcs_bucket)
-        elif source == 'kaggle':
-            kaggle_commands = [
-                    'kaggle competitions download -c grasp-and-lift-eeg-detection',
-                    'kaggle competitions download -c inria-bci-challenge'
+        try:
+
+            success = False
+            if source == 'bnci':
+                success = download_from_bnci("http://bnci-horizon-2020.eu/database/data-sets", destination_folder, args.gcs_bucket)
+            elif source == 'figshare':
+                success = download_from_figshare("https://api.figshare.com/v2/collections/3917698/articles", destination_folder, args.gcs_bucket)
+            elif source == 'mindbigdata':
+                success = download_from_mindnet(destination_folder, args.gcs_bucket)
+            elif source == 'gigadb':
+                success = download_from_gigadb("http://gigadb.org/dataset/view/id/100295", destination_folder, args.gcs_bucket)
+            elif source == 'direct':
+                direct_links = ['https://archive.ics.uci.edu/static/public/457/eeg+steady+state+visual+evoked+potential+signals.zip',
+                'https://www-ti.informatik.uni-tuebingen.de/~spueler/eeg_data/cVEP_dataset.rar',
+                'https://physionet.org/files/eegmmidb/1.0.0/','https://gin.g-node.org/robintibor/high-gamma-dataset/src/master/data/train/1.edf'
+                ,'https://archive.ics.uci.edu/static/public/230/planning+relax.zip',
+                'https://figshare.com/collections/A_large_electroencephalographic_motor_imagery_dataset_for_electroencephalographic_brain_computer_interfaces/3917698',
+                'http://www.enterface.net/enterface05/docs/results/databases/project1_database.zip',
+                'http://www.enterface.net/enterface05/docs/results/databases/project2_database.zip',
+                'http://www.enterface.net/enterface05/docs/results/databases/project5_database.zip',
+                'http://www.enterface.net/enterface05/docs/results/databases/project7_database.zip',
+                'https://headit.ucsd.edu/studies/3316f70e-35ff-11e3-a2a9-0050563f2612.jnlp',
+                'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-access-anchor',
+                'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-iv-access-anchor',
+                'https://bcmi.sjtu.edu.cn/~seed/downloads.html#seed-vig-access-anchor',
+                'https://stacks.stanford.edu/file/druid:xd109qh3109/fhpred.zip',
+                "https://utexas.box.com/shared/static/7ab8qm5e3i0vfsku0ee4dc6hzgeg7nyh.zip",
+                'https://utexas.box.com/shared/static/3go1g4gcdar2cntjit2knz5jwr3mvxwe.zip',
+                "https://openneuro.org/datasets/ds003020/",
+                "https://utexas.box.com/shared/static/ae5u0t3sh4f46nvmrd3skniq0kk2t5uh.zip",
+                "https://openneuro.org/datasets/ds004510/",
+                "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz",
+                "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz"
                 ]
-            success = download_kaggle_datasets(kaggle_commands, destination_folder, args.gcs_bucket)
+                success = download_direct_links(direct_links, destination_folder, args.gcs_bucket)
+            elif source == 'kaggle':
+                kaggle_commands = [
+                        'kaggle competitions download -c grasp-and-lift-eeg-detection',
+                        'kaggle competitions download -c inria-bci-challenge'
+                    ]
+                success = download_kaggle_datasets(kaggle_commands, destination_folder, args.gcs_bucket)
 
-        if success:
-            print(f"[SUCCESS] Completed downloading all files from {args.source}.")
+            if success:
+                    print(f"[SUCCESS] Completed downloading all files from {source}.")
+            else:
+                print(f"[FAILURE] Failed to download all files from {source}.")
+                failed_sources.append(source)
 
+        except Exception as e:
+            print(f"[ERROR] An exception occurred while downloading from {source}: {e}")
+            failed_sources.append(source)
+
+        if failed_sources:
+            print(f"[SUMMARY] Failed to download from the following sources: {', '.join(failed_sources)}")
+            # Optionally, write the failed_sources list to a log file
+            with open("failed_sources.log", "w") as f:
+                for source in failed_sources:
+                    f.write(f"{source}\n")
 if __name__ == '__main__':
     main()
--- modules/gigadb_module.py
+++ modules/gigadb_module.py
@@ -1,5 +1,7 @@
-from extract.downloader import  download_all_files,prepare_download,handle_pagination
+from extract.downloader import download_all_files, prepare_download, handle_pagination
 
-def download_from_gigadb(base_url, destination_folder):
+# Assuming this is in gigadb_module.py or wherever you've defined download_from_gigadb
+
+def download_from_gigadb(base_url, destination_folder, gcs_bucket=None):
     full_path, downloaded_files = prepare_download(destination_folder, "GigaDB_Downloads")
-    return handle_pagination(base_url, "Sample_page", download_all_files, full_path, downloaded_files, 'table.table.table-bordered tbody tr td a[href^="https://ftp.cngb.org"]')
+    return handle_pagination(base_url, "Sample_page", download_all_files, full_path, downloaded_files, 'table.table.table-bordered tbody tr td a[href^="https://ftp.cngb.org"]', gcs_bucket)
