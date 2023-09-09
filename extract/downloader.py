f7bfd5ea14a48f75bb2502c103de4dd5127ca120
Hewlbern <michael.t.holborn@gmail.com>
2023-09-09 12:10:19 +1000

    pagination

--- extract/downloader.py
+++ extract/downloader.py
@@ -109,3 +109,30 @@ def fetch_bnci_links(base_url):
 
     return [urljoin(base_url, link.get('href')) for link in soup.find_all('a') if link.get('href', '').endswith('.mat')]
 
+
+def handle_pagination(base_url, page_param, download_all_files_func, full_path, downloaded_files, link_selector):
+    page_number = 1
+    while True:
+        page_url = f"{base_url}/{page_param}/{page_number}"
+        print(f"Scraping page: {page_url}")
+
+        try:
+            response = requests.get(page_url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+        except requests.RequestException as e:
+            print(f"[ERROR] Failed to fetch {page_url}. Error: {e}")
+            return False
+
+        download_links = [link['href'] for link in soup.select(link_selector)]
+
+        if not download_all_files_func(download_links, full_path, downloaded_files):
+            return False
+
+        next_page_link = soup.select_one('ul.yiiPager li.next a')
+        if next_page_link:
+            page_number += 1
+        else:
+            print("Completed downloading all files.")
+            break
+    return True
