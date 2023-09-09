473139088a2572d38f935deef124addb76234d4a
Hewlbern <michael.t.holborn@gmail.com>
2023-09-09 12:14:08 +1000

    optional

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from extract.downloader import create_folder_if_not_exists
 def main():
     parser = argparse.ArgumentParser(description='Download datasets.')
     parser.add_argument('--source', choices=['gigadb', 'mindbigdata', 'bnci', 'figshare', 'direct', 'kaggle'], default='all', help='Source to download from')
-    parser.add_argument('--gcs_bucket', type=str, help='Google Cloud Storage bucket to upload files to')
+    parser.add_argument('--gcs_bucket', type=str, default=None, help='Google Cloud Storage bucket to upload files to (optional)')
     args = parser.parse_args()
 
     destination_folder = "Downloaded_Datasets"
@@ -18,14 +18,20 @@ def main():
 
     success = False
 
+    # Create a dictionary to hold optional arguments
+    optional_args = {}
+    if args.gcs_bucket:
+        optional_args['gcs_bucket'] = args.gcs_bucket
+
+
     if args.source == 'bnci':
-        success = download_from_bnci("http://bnci-horizon-2020.eu/database/data-sets", destination_folder, args.gcs_bucket)
+        success = download_from_bnci("http://bnci-horizon-2020.eu/database/data-sets", destination_folder, **optional_args)
     elif args.source == 'figshare':
-        success = download_from_figshare("https://api.figshare.com/v2/collections/3917698/articles", destination_folder, args.gcs_bucket)
+        success = download_from_figshare("https://api.figshare.com/v2/collections/3917698/articles", destination_folder, **optional_args)
     elif args.source == 'mindbigdata':
-        success = download_from_mindnet(destination_folder, args.gcs_bucket)
+        success = download_from_mindnet(destination_folder, **optional_args)
     elif args.source == 'gigadb':
-        success = download_from_gigadb("http://gigadb.org/dataset/view/id/100295", destination_folder, args.gcs_bucket)
+        success = download_from_gigadb("http://gigadb.org/dataset/view/id/100295", destination_folder, **optional_args)
     elif args.source == 'direct':
         direct_links = ['https://archive.ics.uci.edu/static/public/457/eeg+steady+state+visual+evoked+potential+signals.zip',
         'https://www-ti.informatik.uni-tuebingen.de/~spueler/eeg_data/cVEP_dataset.rar',
@@ -49,13 +55,13 @@ def main():
         "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz",
         "https://utexas.box.com/s/ri13t06iwpkyk17h8tfk0dtyva7qtqlz"
         ]
-        success = download_direct_links(direct_links, destination_folder, args.gcs_bucket)
+        success = download_direct_links(direct_links, destination_folder, **optional_args)
     elif args.source == 'kaggle':
         kaggle_commands = [
             'kaggle competitions download -c grasp-and-lift-eeg-detection',
             'kaggle competitions download -c inria-bci-challenge'
         ]
-        success = download_kaggle_datasets(kaggle_commands, destination_folder, args.gcs_bucket)
+        success = download_kaggle_datasets(kaggle_commands, destination_folder, **optional_args)
 
     if success:
         print(f"[SUCCESS] Completed downloading all files from {args.source}.")
